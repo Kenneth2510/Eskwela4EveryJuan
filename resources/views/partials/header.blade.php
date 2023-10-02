f41a187f77bb998f62ba4ae0e8bc92c9de721a59
xrai1210 <garciaraven62@gmail.com>
2023-10-02 18:01:22 +0800

    header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -22,6 +22,8 @@
     <script src="{{asset('js/instructorLogin.js')}}" defer></script>
     <script src="{{asset('js/instructorRegister.js')}}" defer></script>
     <script src="{{asset('js/AD_SB.js')}}" defer></script>
+    <script src="{{asset('js/instructorCreateCourse.js')}}" defer></script>
+    <script src="{{asset('js/learnerUserSettings.js')}}" defer></script>
 </head>
 <body class="min-h-full bg-mainwhitebg font-poppins">
     <x-message />
