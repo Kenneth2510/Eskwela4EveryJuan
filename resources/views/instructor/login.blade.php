0fb1a08ed685ed78752e13f5f0c6be430787f5f8
raigrc <ravenjstn.grc@gmail.com>
2024-02-24 02:27:36 +0800

    style login instructor

--- resources/views/instructor/login.blade.php
+++ resources/views/instructor/login.blade.php
@@ -1,8 +1,8 @@
 @include('partials.header')
 
-    <section class="flex flex-row w-full h-screen text-sm ">
+    <section class="flex flex-row w-full h-screen text-base ">
         
-        <header class="fixed top-0 left-0 z-40 flex flex-row items-center w-full px-4 py-4 bg-transparent">
+        <header class="absolute top-0 left-0 z-40 flex flex-row items-center w-full px-4 py-4 bg-transparent">
             <a href="#">
                 <span class="self-center font-semibold font-semibbold whitespace-nowrap md:text-2xl text-darthmouthgreen">
 
@@ -13,20 +13,20 @@
         
         {{-- MAIN --}}
         <div class="relative w-full h-screen text-black lg:overflow-auto md:flex md:justify-center md:items-center md:bg-mainwhitebg lg:w-1/2 lg:text-mainwhitebg lg:pt-24" id="loginForm">
-            <div class="rounded-lg md:shadow-xl md:w-3/4 md:mx-auto md:bg-mainwhitebg lg:bg-opacity-0 lg:shadow-transparent">
+            <div class="max-w-screen-xl pt-16 mx-auto rounded-lg md:shadow-xl md:w-3/4 md:mx-auto md:bg-mainwhitebg lg:bg-opacity-0 lg:shadow-transparent lg:pt-10">
                 {{-- <x-header title="Instructor Login"> --}}
                     {{-- <p class="text-sm md:text-base">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam quidem nobis quasi porro odio! Iusto, aliquam.</p> --}}
                 {{-- </x-header> --}}
 
-                <h1 class="text-6xl font-bold text-darthmouthgreen">Instructor Login</h1>
-                <p class="mt-3 text-sm text-darthmouthgreen md:text-base">Welcome, instructor! Please enter your credentials to access the teaching platform.</p>
+                <h1 class="text-4xl font-bold text-center text-darthmouthgreen">Instructor Login</h1>
+                <p class="mt-3 text-darthmouthgreen">Welcome, instructor! Please enter your credentials to access the teaching platform.</p>
 
                 <form class="flex flex-col justify-center text-black rounded-lg md:mt-4 h-96 md:w-3/4 md:mx-auto lg:w-full" action="{{ url('/instructor/login') }}" method="POST">
                     @csrf
-                    <div class="pb-4 mx-4 text-sm border-b-4">
+                    <div class="pb-4 mx-4 border-b-4">
 
                         <div class="flex flex-col my-4 lg:flex-row lg:justify-between lg:items-center">
-                            <label class="text-xl font-medium text-darthmouthgreen lg:w-1/2" for="instructor_username">Username:</label>
+                            <label class="font-medium text-darthmouthgreen lg:w-1/2" for="instructor_username">Username:</label>
                             <div class="relative lg:w-1/2">
                                 <svg class="absolute w-8 h-8 mx-1 border-r-2 md:w-10 md:h-10 lg:my-1" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h640q33 0 56.5 23.5T880-720v480q0 33-23.5 56.5T800-160H160Zm320-280L160-640v400h640v-400L480-440Zm0-80 320-200H160l320 200ZM160-640v-80 480-400Z"/></svg>
 
@@ -43,13 +43,13 @@
                         </div>
                         
                         <div class="flex flex-col my-4 lg:flex-row lg:justify-between lg:items-center">
-                            <label class="text-xl font-medium text-darthmouthgreen lg:w-1/2" for="instructor_password">Password:</label>
+                            <label class="font-medium text-darthmouthgreen lg:w-1/2" for="instructor_password">Password:</label>
                             <div class="relative lg:w-1/2">
                                 <svg class="absolute w-8 h-8 mx-1 border-r-2 md:w-10 md:h-10 lg:my-1" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M240-80q-33 0-56.5-23.5T160-160v-400q0-33 23.5-56.5T240-640h40v-80q0-83 58.5-141.5T480-920q83 0 141.5 58.5T680-720v80h40q33 0 56.5 23.5T800-560v400q0 33-23.5 56.5T720-80H240Zm0-80h480v-400H240v400Zm240-120q33 0 56.5-23.5T560-360q0-33-23.5-56.5T480-440q-33 0-56.5 23.5T400-360q0 33 23.5 56.5T480-280ZM360-640h240v-80q0-50-35-85t-85-35q-50 0-85 35t-35 85v80ZM240-160v-400 400Z"/></svg>
                                 <svg class="absolute right-0 w-6 h-6 mx-1 top-1 lg:w-8 lg:h-8" id="showPwd" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M480-320q75 0 127.5-52.5T660-500q0-75-52.5-127.5T480-680q-75 0-127.5 52.5T300-500q0 75 52.5 127.5T480-320Zm0-72q-45 0-76.5-31.5T372-500q0-45 31.5-76.5T480-608q45 0 76.5 31.5T588-500q0 45-31.5 76.5T480-392Zm0 192q-146 0-266-81.5T40-500q54-137 174-218.5T480-800q146 0 266 81.5T920-500q-54 137-174 218.5T480-200Zm0-300Zm0 220q113 0 207.5-59.5T832-500q-50-101-144.5-160.5T480-720q-113 0-207.5 59.5T128-500q50 101 144.5 160.5T480-280Z"/></svg>
                                 <svg class="absolute right-0 hidden w-6 h-6 mx-1 top-1 lg:my-1" id="hidePwd" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="m644-428-58-58q9-47-27-88t-93-32l-58-58q17-8 34.5-12t37.5-4q75 0 127.5 52.5T660-500q0 20-4 37.5T644-428Zm128 126-58-56q38-29 67.5-63.5T832-500q-50-101-143.5-160.5T480-720q-29 0-57 4t-55 12l-62-62q41-17 84-25.5t90-8.5q151 0 269 83.5T920-500q-23 59-60.5 109.5T772-302Zm20 246L624-222q-35 11-70.5 16.5T480-200q-151 0-269-83.5T40-500q21-53 53-98.5t73-81.5L56-792l56-56 736 736-56 56ZM222-624q-29 26-53 57t-41 67q50 101 143.5 160.5T480-280q20 0 39-2.5t39-5.5l-36-38q-11 3-21 4.5t-21 1.5q-75 0-127.5-52.5T300-500q0-11 1.5-21t4.5-21l-84-82Zm319 93Zm-151 75Z"/></svg>
                                 
-                                <input required class="w-full h-8 pl-10 rounded text-darthmouthgreen md:h-10 lg:h-12 md:pl-12" type="password" name="password" id="password">
+                                <input required class="w-full h-8 pl-10 rounded text-darthmouthgreen md:h-10 lg:h-12 md:pl-12 ring-seagreen ring-2" type="password" name="password" id="password">
 
 
                             </div>
@@ -75,7 +75,7 @@
                             @enderror
                         @endif
                         
-                        <div class="flex items-center justify-between px-4 my-4 md:text-base">
+                        <div class="flex items-center justify-between px-4 my-4">
                             <div class="flex items-center">
                                 <input class="w-4 h-4 mx-1 accent-darthmouthgreen" type="checkbox" name="remember" id="remember">
                                 <label class="text-darthmouthgreen" for="remember">Remember me</label>
@@ -83,12 +83,11 @@
                             
                             
                             {{-- <x-forms.primary-button color="amber" name="Log in" /> --}}
-                            <button type="submit" name="Login" class="px-5 py-3 text-xl text-white rounded-xl bg-darthmouthgreen hover:bg-white hover:text-darthmouthgreen hover:border-darthmouthgreen hover:border-2">Login</button>
+                            <button type="submit" name="Login" class="px-5 py-3 text-white rounded-xl bg-darthmouthgreen hover:bg-white hover:text-darthmouthgreen hover:border-darthmouthgreen hover:ring-2 hover:ring-darthmouthgreen">Login</button>
                         </div>
                     </div>
                 </form>
-                <div class="mx-auto my-10 text-sm w-max md:text-base">
-
+                <div class="w-full text-center">
                     {{--<p class="text-black lg:text-mainwhitebg">Don't have an account yet?
                         <span class="font-semibold text-darthmouthgreen lg:text-white">
                             <a href="{{url('/instructor/register1')}}">--}}
